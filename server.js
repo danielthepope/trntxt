a6c805fbad6889a31f780f2c02d50bf55d182c16
Daniel Pope <danielthepope@users.noreply.github.com>
2015-03-24 00:28:10 +0000

    Added express

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http');
+var express = require('express');
 var fs = require('fs');
 var util = require('util');
 
@@ -7,20 +7,27 @@ var apiKey = process.env.APIKEY;
 var apiUser = process.env.APIUSER;
 var stationCsv = process.env.STATIONCSV || 'resources/station_codes.csv';
 
-var stations = getStations();
-var i;
-
-http.createServer(function (req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.write(util.format('Station CSV: %s\n', stationCsv));
-    for (i = 0; i < stations.length; i += 1) {
-        res.write(util.format('%s\n', stations[i]));
-    }
-	res.end('One day (towards the end of April) this will be a really swish train times website that will work over GPRS.\n');
-}).listen(port);
-
 function getStations() {
     var result = fs.readFileSync(stationCsv, 'utf8').split('\n');
     result = result.slice(1, result.length);
     return result;
 }
+
+var app = express();
+var stations = getStations();
+
+var i;
+ 
+app.get('/', function (req, res) {
+    res.set('Content-Type', 'text/plain');
+    var content = util.format('Station CSV: %s\n', stationCsv);
+    for (i = 0; i < stations.length; i += 1) {
+        content += util.format('%s\n', stations[i]);
+    }
+    content += 'One day (towards the end of April) this will be a really swish train times website that will work over GPRS.\n';
+    res.send(content);
+});
+ 
+var server = app.listen(port, function () {
+    console.log('listening on port %s', port);
+});
