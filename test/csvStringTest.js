67e467a3f5a4cdd9e6f72beaa254cb63e33eab59
Daniel Pope <danielthepope@users.noreply.github.com>
2015-07-21 19:23:23 +0100

    Added a couple of tests to check that csv-string does what I want it to

---
+++ test/csvStringTest.js
@@ -0,0 +1,20 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var csv = require('csv-string');
+
+describe('Checking inputs with quotes', function() {
+	it('Should ignore quotes', function() {
+		var output = csv.parse('"hello world",data');
+		expect(output).to.be.an('array').and.have.length(1);
+		expect(output[0]).to.be.an('array').and.have.length(2);
+		expect(output[0][0]).to.equal('hello world');
+	});
+	
+	it('Should ignore commas within quotes', function() {
+		var output = csv.parse('"hello, world",data');
+		expect(output).to.be.an('array').and.have.length(1);
+		expect(output[0]).to.be.an('array').and.have.length(2);
+		expect(output[0][0]).to.equal('hello, world');
+	});
+});
