0e2e3cb06c5aade3ea1be147767e841aadb9fb8f
Dan Pope <danielthepope@users.noreply.github.com>
2018-05-28 23:47:00 +0100

    Make tests point at dist directory

--- test/integration/integrationTest.js
+++ test/integration/integrationTest.js
@@ -4,7 +4,7 @@ const Browser = require('zombie');
 const crypto = require('crypto');
 const fs = require('fs');
 
-const server = require('../../src/server');
+const server = require('../../dist/server');
 
 function urlFor(path) {
   if (!path.startsWith('/')) {
