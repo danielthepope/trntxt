27f8e471356770f3c9ace2710b3a64b5dfc3180e
Christian Lawson-Perfect <christianperfect@gmail.com>
2025-05-03 10:53:05 +0100

    update stations
    The station picker gives an error unless you set the referer and origin
    headers to nationalrail.co.uk.
    
    This also changes the stationFetcher.sh script to use `python3` instead
    of `python`.

--- resources/stationFetcher/stationFetcher.sh
+++ resources/stationFetcher/stationFetcher.sh
@@ -4,6 +4,8 @@ set -eu
 
 cd $(dirname $0)
 mkdir -p tmp
-for LETTER in a b c d e f g h i j k l m n o p q r s t u v w x y z; do curl -s https://stationpicker.nationalrail.co.uk/stationPicker/$LETTER > tmp/$LETTER.json; done
-python jsonProcessor.py
+for LETTER in a b c d e f g h i j k l m n o p q r s t u v w x y z; do 
+    curl https://stationpicker.nationalrail.co.uk/stationPicker/$LETTER -H 'Accept: */*' -H 'Referer: https://www.nationalrail.co.uk/' -H 'Origin: https://www.nationalrail.co.uk' > tmp/$LETTER.json
+done
+python3 jsonProcessor.py
 rm -r tmp
