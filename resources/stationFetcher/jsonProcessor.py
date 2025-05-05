209c7a9fe6e9f88fd50439f00ea19ef044d13958
Dan Pope <danielthepope@users.noreply.github.com>
2025-05-05 19:57:22 +0100

    Remove hoverports, copy into station_codes.csv

--- resources/stationFetcher/jsonProcessor.py
+++ resources/stationFetcher/jsonProcessor.py
@@ -27,7 +27,7 @@ stations = set()
 for letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
     data = json.load(open(f'tmp/{letter}.json'))
     for station in data['payload']['stations']:
-        if CRS_RE.match(station['crsCode']) and station['operator'] != '':
+        if CRS_RE.match(station['crsCode']) and station['darwinState'] == 4 and station['operator'] != '':
             stations.add(Station(station['crsCode'], station['name']))
 
 with open('stations.csv', 'w', newline='') as out_file:
